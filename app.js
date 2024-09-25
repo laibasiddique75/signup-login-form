e42aa59beefcd6d4ab0f44e478afb4f37e20cf04
laibasiddique75 <laibasiddique111@gmail.com>
2024-09-25 06:54:34 +0500

    updated

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-document.getElementById("sign-up").addEventListener("click",function(){
+
+var users=[];
+
+var signUpBtn = document.getElementById("sign-up")
+ signUpBtn && signUpBtn.addEventListener("click",function(){
 
 
    
@@ -9,7 +13,7 @@ document.getElementById("sign-up").addEventListener("click",function(){
         console.log(name.value,password.value,email.value);
     
     
-        var users=[];
+       
         var userObj = {
         name: name.value,
         email:email.value,
@@ -25,9 +29,55 @@ document.getElementById("sign-up").addEventListener("click",function(){
         email.value =''
         password.value =''
         localStorage.clear()
+
         localStorage.setItem("userData",JSON.stringify(users))
+
+
+
+
+
+
+        var loginBtn = document.getElementById("login")
+
+
+        loginBtn.addEventListener("click",function(){
+        
+        
+            var loginEmail = document.getElementById("login-email")
+            var loginPass = document.getElementById("login-password")
+        
+            console.log(loginEmail.value,loginPass.value);
+
+
+
+            var userObject = {
+               
+                email:loginEmail.value,
+                password:loginPass.value
+                }
+                console.log(userObject);
+
+
+
+
+
+    loginEmail.value =''
+    loginPass.value =''
+    
+    
+
+            
+        })
+
+
+
+
+
+
         
     })
 
 
 
+     
+                     
